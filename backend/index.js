108feb5b9414c714452cdc98e312912ae65ff4f2
Sujal Chordia <sujalchordia10@gmail.com>
2023-04-08 14:38:40 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 //middleware
 app.use((req,res,next)=>{
-res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+res.setHeader("Access-Control-Allow-Origin","*");
 res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Control-Type, Accept"
